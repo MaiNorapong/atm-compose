560bebb9504136535e1bd47075d6c3eaf93f552d
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-10-27 19:37:55 +0700

    Add script to initialise database

---
+++ create_db.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE atm;
+USE atm;
+
+CREATE TABLE customer (
+   id INT NOT NULL,
+   name VARCHAR(45) NOT NULL,
+   pin TEXT NOT NULL,
+   PRIMARY KEY (id)
+);
+
+CREATE DATABASE bank;
+USE bank;
+
+CREATE TABLE bank_account (
+   id INT NOT NULL AUTO_INCREMENT,
+   customer_id INT NOT NULL,
+   type VARCHAR(20) NOT NULL,
+   balance DOUBLE NOT NULL,
+   PRIMARY KEY (id)
+);
+
+INSERT INTO bank_account (customer_id, type, balance) VALUES (1, "Saving", 5000);
+INSERT INTO bank_account (customer_id, type, balance) VALUES (1, "Checking", 10000);
+INSERT INTO bank_account (customer_id, type, balance) VALUES (2, "Saving", 200000);
